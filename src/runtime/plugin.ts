93a5794c068a76f6d9c43bcade779558614e1ad6
Jairo <jairoblattc@gmail.com>
2023-11-23 09:22:20 -0300

    Update dependencies and fix import in plugin.ts

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,6 +1,6 @@
 import { defineNuxtPlugin } from '#imports';
 import { consola } from 'consola';
-import { default as Rollbar } from 'rollbar';
+import Rollbar from 'rollbar';
 
 const isValidAccessToken = (accessToken: unknown): accessToken is string => {
   return typeof accessToken === 'string' && accessToken.length > 0;
